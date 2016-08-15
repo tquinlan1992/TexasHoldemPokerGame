ad1ab0e5aba14d87b700d2e098289b1c0b5ada42
Thomas Quinlan <tquinlan1992@gmail.com>
2016-08-14 21:38:53 -0400

    set up browserify, babelify, uglify for PokerGame

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,49 @@ var gulp = require('gulp');
 var tape = require('gulp-tape');
 var tapColorize = require('tap-colorize');
 var jshint = require('gulp-jshint');
-var packageJSON  = require('./package');
+var packageJSON = require('./package');
 var jshintConfig = packageJSON.jshintConfig;
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 gulp.task('test', function() {
-  return gulp.src('test/*.js')
-    .pipe(tape({
-      reporter: tapColorize()
-    }));
+    return gulp.src('test/*.js')
+        .pipe(tape({
+            reporter: tapColorize()
+        }));
 });
 
 gulp.task('jshint', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(jshint(jshintConfig))
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
+    return gulp.src('src/**/*.js')
+        .pipe(jshint(jshintConfig))
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
 });
+
+gulp.task('build', function() {
+    // app.js is your main JS file with all your module inclusions
+    return browserify({
+            entries: './src/pokerGame/PokerGame.js',
+            debug: true
+        })
+        .transform("babelify", {
+            presets: ["es2015"]
+        })
+        .bundle()
+        .pipe(source('PokerGame.min.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('watch', ['build'], function() {
+    gulp.watch('./src/js/*.js', ['build']);
+});
+
 
 gulp.task('default', ['jshint', 'test']);
